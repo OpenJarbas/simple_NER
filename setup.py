7cf06aeeba1873dcc25cb67cab75eeffe03e8e78
jarbasal <jarbasai@mailfence.com>
2019-04-04 02:23:35 +0100

    pip install

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('little_questions')
 
 setup(
     name='simple_NER',
-    version='0.1.8.1',
+    version='0.1.8.2',
     packages=['simple_NER', 'simple_NER.rules', 'simple_NER.annotators',
               'simple_NER.annotators.utils',
               'simple_NER.annotators.utils.keywords'],
     url='https://github.com/JarbasAl/simple_NER',
+    package_data={'': extra_files},
+    include_package_data=True,
     license='MIT',
     author='jarbasAI',
     install_requires=["padaos", "fann2==1.0.7", "padatious", "nltk",
